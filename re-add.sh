2b466c2099dc187af0ab864c0d12e0a4ef4e0ad9
Rizqi Kamal <rizqi@example.com>
2025-07-16 19:50:01 +0000

    Auto backup: 2025-07-16 19:50:01

--- re-add.sh
+++ re-add.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Array of IP addresses
-declare -a ip_addresses=("34.86.148.180" "34.145.201.163")
+declare -a ip_addresses=("34.9.212.108" "34.29.125.177")
 
 # Base domain name and timestamp
 base_domain="ayoyo-studentart.fun"
